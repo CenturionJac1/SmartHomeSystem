fff3674bc355679656b249ae9f76208e15c04e78
Jacob Fowles <u1437247@utah.edu>
2026-09-01 14:25:39 -0600

    Created starter file structure for firmware, hardware, website, docs, and tests. Mostly empty files, README not updated.

---
+++ firmware/humidifier/include/config.h
@@ -0,0 +1,7 @@
+//This is where all configurations that are NOT secret are kept for the firmware. for example:
+
+//GPIO assignments
+
+//Network settings (NOT PASSWORDS)
+
+//Device Behavior (defaults)
